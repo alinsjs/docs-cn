59b6d16ada828c65ec98607d38eea1b503b84ef3
theajack <1506282385@qq.com>
2022-11-12 16:59:22 +0800

    feat: dest

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     description: 'Alins - All-in-JS Web FrameWork', // 描述
     keywords: 'alins,all-in-js,css-in-js,web-framework',
-    dest: './docs-cn/', // 基本url
+    dest: './docs/', // 打包地址
     base: '/docs-cn/', // gh-pages分支这里需要改成 / 因为 cnchar.js.org的配置
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
